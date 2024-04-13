64f0e2ebdde26bda4dfd3c31879425119d6ae6a2
cnfairall <101303134+cnfairall@users.noreply.github.com>
2024-04-13 11:55:14 -0500

    forgot to save

--- Api/ItemsApi.cs
+++ Api/ItemsApi.cs
@@ -1,6 +1,24 @@
-﻿namespace HHPWServer.Api
+﻿using HHPWServer.Models;
+namespace HHPWServer.Api
 {
     public class ItemsApi
     {
+        public static void Map(WebApplication app)
+        {
+            app.MapGet("/api/items", (HHPWServerDbContext db) =>
+            {
+                return db.Items;
+            });
+
+            app.MapGet("/api/items/{itemId}", (HHPWServerDbContext db, int itemId) =>
+            {
+                Item singleItem = db.Items.SingleOrDefault(i => i.Id == itemId);
+                if (singleItem != null)
+                {
+                    return singleItem;
+                }
+                return null;
+            });
+        }
     }
 }
